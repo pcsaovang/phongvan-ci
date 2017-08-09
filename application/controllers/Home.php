1f9e2f8a423e154c1d99a570c15664995a88ec99
Tran Phong <tranphong.dev@gmail.com>
2017-08-09 15:31:11 +0700

    Add controller Home

---
+++ application/controllers/Home.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by Tran Phong.
+ * Date: 8/9/2017 3:30 PM
+ * Filename: Home.php
+ */
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+    public function index()
+    {
+        echo 1;
+    }
+}
